9b54b434e5415a7aa75ae1df2ec629da35c39dbe
Lois Talagrand <lois.talagrand@gmail.com>
2020-01-03 09:13:24 -1000

    Read video length

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const electron = require("electron");
+const ffmpeg = require("ffmpeg");
+const ffmpeg = require('fluent-ffmpeg');
+
 
 // app is the overall running process
-const { app } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on("ready", () => {
-  console.log("hello world!");
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on("video:submit", (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log("Test");
+    console.log("Error: ", err);
+    console.log("Video duration is: ", metadata.format.duration);
+  });
 });
